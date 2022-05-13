768af9133d81d80d3481da18d0ccc5c2bf39ad36
kenia lorena panduro chavez <kl.panduroc@alum.up.edu.pe>
2022-05-13 13:02:39 -0500

    agregar ahora

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateReservaComponent } from './components/create-reserva/create-reserva.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProgramaGrupalPeruComponent } from './components/programaGrupalPeru/programaGrupalPeru.component';
 import { ProgramaIndividualPeruComponent } from './components/programaIndividualPeru/programaIndividualPeru.component';
 import { ReservaFinalComponent } from './components/reserva-final/reserva-final.component';
 
@@ -9,7 +10,8 @@ const routes: Routes = [
 
   /*{path:'create-reserva',component:CreateReservaComponent},*/
   {path:'fin-reserva', component: ReservaFinalComponent},
-  {path:'individual-peru', component: ProgramaIndividualPeruComponent}
+  {path:'individual-peru', component: ProgramaIndividualPeruComponent},
+  {path:'grupal-peru', component: ProgramaGrupalPeruComponent }
 ];
 
 @NgModule({
